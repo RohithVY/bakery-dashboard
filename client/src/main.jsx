1291694298f8fa6a9c0939f83f828654de6cb543
RohithVY <venkatrohith1998@gmail.com>
2023-11-09 21:24:11 +0530

    removed dev tools

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import "@/index.css";
 import store from "./app/store.jsx";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <App />
-        <ReactQueryDevtools initialIsOpen={true} />
+        {/* <ReactQueryDevtools initialIsOpen={true} /> */}
       </Provider>
     </QueryClientProvider>
   </React.StrictMode>
